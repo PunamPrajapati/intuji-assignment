82f69b0bde7283f46884a5f0959bed443c9dbe5f
PunamPrajapati <poonamprajapati96.pp@gmail.com>
2024-06-11 22:15:20 +0545

    implemented google calendar in project

--- config/config.php
+++ config/config.php
@@ -9,7 +9,10 @@
     if(!session_id()) session_start(); 
 
     // Google API configuration 
-    
+    define('GOOGLE_CLIENT_ID', '531272548723-c10nvch722hjd0id57dmnb2m4btohmg2.apps.googleusercontent.com'); 
+    define('GOOGLE_CLIENT_SECRET', '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'); 
+    define('GOOGLE_OAUTH_SCOPE', 'https://www.googleapis.com/auth/calendar'); 
+    define('REDIRECT_URI', 'http://localhost/intuji-assignment/controllers/google_calendar_event_sync.php'); 
 
     // $googleOauthURL = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode(GOOGLE_OAUTH_SCOPE) . '&redirect_uri=' . REDIRECT_URI . '&response_type=code&client_id=' . GOOGLE_CLIENT_ID . '&access_type=online'; 
 ?>
